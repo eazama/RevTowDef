9ea4de4aa1a81b2924a599785c4de5ef6dca458c
x3jasmine <x3_momoko@hotmail.com>
2014-05-31 22:42:33 -0700

    Added a Menu scene
    Buttons don't do anything yet.

---
+++ Assets/Resources/Scripts/MenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	public int x, y, sizeX, sizeY;
+	public Texture playButton;
+	public Texture creditsButton;
+	public Texture howToPlayButton;
+	public GameObject circle;
+	int i = 0;
+	// Use this for initialization
+	void Start () {
+
+		sizeX = playButton.width;
+		sizeY = playButton.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i++;
+		circle.transform.eulerAngles = new Vector3 (0, 0, -i/2);
+	}
+
+	void OnGUI () {
+		GUI.backgroundColor = new Color(0,0,0,0);
+		if (GUI.Button (new Rect (10, 5, sizeX, sizeY), playButton)) 
+		{
+			//Application.LoadLevel (Application.loadedLevel + 1);
+			//print ("reset!!");
+		}
+
+		if (GUI.Button (new Rect (10, sizeX/2+30, sizeX, sizeY), howToPlayButton)) 
+		{
+			//Application.LoadLevel (Application.loadedLevel + 1);
+			//print ("reset!!");
+		}
+
+		if (GUI.Button (new Rect (10, sizeX+55, sizeX, sizeY), creditsButton)) 
+		{
+			//Application.LoadLevel (Application.loadedLevel + 1);
+			//print ("reset!!");
+		}
+	}
+}
