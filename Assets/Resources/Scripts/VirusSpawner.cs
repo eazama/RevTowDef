7815d57c9e56cc516e63094969fb9342795d0f0a
x3jasmine <x3_momoko@hotmail.com>
2014-06-04 21:40:45 -0700

    Minor changes to shop to include price
    Also deducts resources

--- Assets/Resources/Scripts/VirusSpawner.cs
+++ Assets/Resources/Scripts/VirusSpawner.cs
@@ -5,24 +5,42 @@ public class VirusSpawner:MonoBehaviour{
 
 	public static void spawn(int i){
 		GameObject obj;
-		switch(i){
-		case 0:
-			Debug.Log("SPAWNING BASIC");
-			obj = Instantiate (Resources.Load("Prefabs/BasicVirus")) as GameObject;
-			BasicMovement move = obj.GetComponent<BasicMovement>();
-			move.getPath (GameObject.FindGameObjectWithTag("Goal"));
-			break;
-		case 1:
-			Debug.Log("SPAWNING Wallbreaker");
-			obj = Instantiate (Resources.Load("Prefabs/WallBreakVirus") ) as GameObject;
-			break;
-		case 2:
-			Debug.Log("SPAWNING Bomber");
-			obj = Instantiate (Resources.Load ("Prefabs/BomberTarget")) as GameObject;
-			BomberTarget tar = obj.GetComponent<BomberTarget>();
-			tar.thisHolding = true;
-			BomberTarget.holding = true;
-			break;
+		if (Shop.cashCount > 0) {
+			switch (i) {
+			case 0:
+				if (Shop.cashCount >= 5) {
+					Debug.Log ("SPAWNING BASIC");
+					obj = Instantiate (Resources.Load ("Prefabs/BasicVirus")) as GameObject;
+					BasicMovement move = obj.GetComponent<BasicMovement> ();
+					move.getPath (GameObject.FindGameObjectWithTag ("Goal"));
+					Shop.cashCount = Shop.cashCount - 5;
+				}
+				break;
+			case 1:
+				if (Shop.cashCount >= 25) {
+					Debug.Log ("SPAWNING Wallbreaker");
+					obj = Instantiate (Resources.Load ("Prefabs/WallBreakVirus")) as GameObject;
+					Shop.cashCount = Shop.cashCount - 25;
+				}
+				break;
+			case 2:
+				if (Shop.cashCount >= 50) {
+					Debug.Log ("SPAWNING Bomber");
+					obj = Instantiate (Resources.Load ("Prefabs/BomberTarget")) as GameObject;
+					BomberTarget tar = obj.GetComponent<BomberTarget> ();
+					tar.thisHolding = true;
+					BomberTarget.holding = true;
+					Shop.cashCount = Shop.cashCount - 50;
+				}
+				break;
+			case 3:
+				if (Shop.cashCount >= 60) {
+					Debug.Log("SPAWNING Resource Tower");
+					Instantiate (Resources.Load("Prefabs/ResourceTower"));
+					Shop.cashCount = Shop.cashCount - 60;
+				}
+				break;
+			}
 		}
 	}
 }
