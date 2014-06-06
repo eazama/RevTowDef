fb8e933484d64ce88224939b1504f0b5a0bb5b6f
eazama <eazama001@gmail.com>
2014-06-06 02:21:06 -0700

    Temp Patch on a Temp AI

--- Assets/Resources/Scripts/InterferenceAI.cs
+++ Assets/Resources/Scripts/InterferenceAI.cs
@@ -40,37 +40,29 @@ public class InterferenceAI : MonoBehaviour
 				}
 			}
 
-			//Get an object tagged with "Player"
-			GameObject closest = GameObject.FindGameObjectWithTag ("Player");
-			//As long as there is one "Player" object
-			if (closest != null) {
-				//find the closest one
-				foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player")) {
-					if (Vector3.Distance (this.transform.position, obj.transform.position) < Vector3.Distance (this.transform.position, closest.transform.position)) {
-						closest = obj;
-					}
-				}
-				//If the player object has a path
-				if (closest.GetComponent<BasicMovement> ().path != null) {
-					GridCoord[] path = closest.GetComponent<BasicMovement> ().path.ToArray ();
-					//that is not empty
-					if (path.Length > 0) {
-						//get a random tile from that path
-						GridCoord location = path [Mathf.FloorToInt (Random.value * path.Length)];
-						//if the position is not on the same row or column
-						if (location.x != transform.position.x && location.y != transform.position.y) {
-							//place a barrier
-							(Instantiate (Resources.Load ("Prefabs/Barrier"), new Vector3 (location.x, location.y, 1), new Quaternion ()) as GameObject).transform.eulerAngles = new Vector3 (0, 180, 0);
-							Pathfinder.grid [location.x, location.y] = true;
-							//and update the paths for every player object
-							foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player")) {
-								obj.GetComponent<BasicMovement> ().getPath (GameObject.FindGameObjectWithTag ("Goal"));
+			GridCoord[] path = Pathfinder.FindPath (new GridCoord (2, 2),
+			                                        new GridCoord (Mathf.RoundToInt (transform.position.x),
+			              										Mathf.RoundToInt (transform.position.y))).ToArray();
 
-							}
+
+			if (path.Length > 0) {
+				//get a random tile from that path
+				GridCoord location = path [Mathf.FloorToInt (Random.value * path.Length)];
+				//if the position is not on the same row or column
+				if (location.x != transform.position.x && location.y != transform.position.y) {
+					//place a barrier
+					(Instantiate (Resources.Load ("Prefabs/Barrier"), new Vector3 (location.x, location.y, 1), new Quaternion ()) as GameObject).transform.eulerAngles = new Vector3 (0, 180, 0);
+					Pathfinder.grid [location.x, location.y] = true;
+					//and update the paths for every player object
+					foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Player")) {
+						if(obj.GetComponent<BasicMovement>() != null){
+							obj.GetComponent<BasicMovement> ().getPath (GameObject.FindGameObjectWithTag ("Goal"));
 						}
 					}
 				}
+
 			}
+			
 			//wait for 1 second before continuing
 			yield return new WaitForSeconds (1);
 		}
